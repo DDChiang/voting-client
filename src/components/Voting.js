41a1c4707b94a3686e3721634a90f3e968eb2b2b
DDCHIANG <ddeechng@gmail.com>
2016-06-24 08:07:32 -0700

    add gitignore

---
+++ src/components/Voting.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import Vote from './Vote';
+import Winner from './Winner';
+
+export default React.createClass({
+  mixins: [PureRenderMixin],
+  getPair: function() {
+    return this.props.pair || [];
+  },
+  isDisabled: function() {
+    return !!this.props.hasVoted;
+  },
+  hasVotedFor: function(entry) {
+    return this.props.hasVoted === entry;
+  },
+  render: function() {
+    return <div className="voting">
+      {this.props.winner ?
+        <Winner ref="winner" winner={this.props.winner} /> :
+        <Vote {...this.props} /> }
+    </div>;
+  }
+});
